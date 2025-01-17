ac7510058d075bb91a0a1844646c784025bb372b
eodudrepublic <eodudrepublic@gmail.com>
2025-01-17 19:26:27 +0900

    feat - kakao 로그인 구현 - 로그인한 사용자의 id, nickname, profile url을 서버로 보내는 기능은 주석처리

---
+++ lib/view/login/login_view.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../common/app_colors.dart';
+import '../../common/utils/logger.dart';
+import '../../service/kakao_login_api.dart';
+import '../../view_model/login/user_controller.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+
+    // KakaoLoginApi 인스턴스를 생성
+    final KakaoLoginApi kakaoLoginApi = KakaoLoginApi();
+
+    // UserController를 의존성 주입
+    final UserController userController =
+        Get.put(UserController(kakaoLoginApi: kakaoLoginApi));
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20.sp),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: statusBarHeight),
+            Expanded(
+                child: Container(
+              alignment: Alignment.topLeft,
+              padding: EdgeInsets.only(top: 0.045.sh),
+              child: Text("서비스 사용을 위해\n로그인이 필요해요",
+                  style: TextStyle(
+                      fontSize: 25.sp,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                  textAlign: TextAlign.start),
+            )),
+            _loginButton(userController),
+            // _logoutButton(userController),
+            SizedBox(height: 0.05.sh),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // 프로필 이미지 위젯
+  Widget _profile(UserController controller) {
+    return Obx(() {
+      if (controller.user.value?.profileImageUrl != null) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 50,
+            backgroundImage:
+                NetworkImage(controller.user.value!.profileImageUrl!),
+          ),
+        );
+      } else {
+        return const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            radius: 50,
+            backgroundColor: Colors.grey,
+            child: Icon(Icons.person, size: 50, color: Colors.white),
+          ),
+        );
+      }
+    });
+  }
+
+  // 닉네임 위젯
+  Widget _nickName(UserController controller) {
+    return Obx(() {
+      if (controller.user.value?.nickname != null) {
+        return Text(
+          controller.user.value!.nickname!,
+          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        );
+      } else {
+        return const Text(
+          "로그인이 필요합니다",
+          style: TextStyle(fontSize: 18),
+        );
+      }
+    });
+  }
+
+  // 로그인 버튼 위젯
+  Widget _loginButton(UserController controller) {
+    return Obx(() {
+      if (controller.user.value?.id == null) {
+        return ElevatedButton(
+          onPressed: () async {
+            await controller.kakaoLogin();
+            if (controller.user.value?.id != null) {
+              Log.info('로그인 성공');
+              // TODO : Get.offNamed('/home');
+            }
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: AppColors.kakaotalkYellow,
+            padding: EdgeInsets.symmetric(horizontal: 20.sp, vertical: 7.sp),
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(12), // 테두리 곡률 설정 : 12 픽셀(Pixel)
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center, // 중앙 정렬
+            mainAxisSize: MainAxisSize.max, // 버튼의 가로 확장
+            children: [
+              Image.asset(
+                'assets/icons/kakao_icon.png',
+                width: 30.sp,
+                height: 30.sp,
+              ),
+              SizedBox(width: 15.sp), // 아이콘과 텍스트 간격
+              Text(
+                '카카오 로그인',
+                style:
+                    TextStyle(color: AppColors.kakaotalkLabel, fontSize: 25.sp),
+              ),
+            ],
+          ),
+        );
+      } else {
+        return Text(
+          '카카오 로그인 완료!',
+          style: TextStyle(color: AppColors.kakaotalkLabel, fontSize: 25.sp),
+        );
+      }
+    });
+  }
+
+  // 로그아웃 버튼 위젯
+  Widget _logoutButton(UserController controller) {
+    return Obx(() {
+      if (controller.user.value?.id != null) {
+        return ElevatedButton(
+          onPressed: () {
+            controller.kakaoLogout();
+          },
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red, // 로그아웃 버튼 색상
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          ),
+          child: const Text(
+            'Logout',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        );
+      } else {
+        return const SizedBox.shrink(); // 로그아웃 버튼 숨김
+      }
+    });
+  }
+}
---
+++ lib/view_model/login/user_controller.dart
@@ -0,0 +1,82 @@
+import 'dart:convert';
+import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
+import '../../common/server_url.dart';
+import '../../common/utils/logger.dart';
+import '../../model/user_model.dart';
+import '../../service/kakao_login_api.dart';
+
+class UserController extends GetxController {
+  // Rx<AppUser?>은 반응형으로 사용자 정보를 저장합니다.
+  var user = Rxn<AppUser>();
+  final KakaoLoginApi kakaoLoginApi;
+
+  UserController({required this.kakaoLoginApi});
+
+  @override
+  void onInit() {
+    super.onInit();
+    // 싱글톤 인스턴스로 초기화
+    user.value = AppUser();
+  }
+
+  // 카카오 로그인 메서드
+  Future<void> kakaoLogin() async {
+    try {
+      var kakaoUser = await kakaoLoginApi.signWithKakao();
+      if (kakaoUser != null) {
+        AppUser appUser = AppUser.fromKakaoUser(kakaoUser);
+        user.value = appUser;
+        Get.snackbar('성공', '카카오 로그인이 성공했습니다.');
+
+        // TODO : 서버 구성되면 서버로 사용자 정보 전송 (현재는 주석)
+        // // Prepare JSON payload
+        // Map<String, dynamic> payload = {
+        //   "user_id": appUser.id.toString(), // 필드 이름 변경
+        //   "nickname": appUser.nickname ?? "",
+        //   "profileImageUrl": appUser.profileImageUrl ?? "", // 필드 이름 변경
+        // };
+        //
+        // // Send POST request to the server
+        // var response = await http.post(
+        //   Uri.parse("$serverUrl:8000/users/login"),
+        //   headers: {"Content-Type": "application/json"},
+        //   body: jsonEncode(payload),
+        // );
+        //
+        // // 응답 확인
+        // Log.info("Status Code: ${response.statusCode}");
+        // Log.info("Response Body: ${response.body}");
+        // Log.info("Payload being sent: ${jsonEncode(payload)}");
+        //
+        // if (response.statusCode == 200 || response.statusCode == 201) {
+        //   var responseData = jsonDecode(response.body);
+        //   String message = responseData["message"];
+        //   Log.info("서버 전송 성공! Server response: $message");
+        //   // Get.snackbar('서버 응답', message);
+        // } else {
+        //   Log.error('서버와의 통신 중 문제가 발생했습니다. 상태 코드: ${response.statusCode}');
+        //   Get.snackbar(
+        //       '서버 오류', '서버와의 통신 중 문제가 발생했습니다. 상태 코드: ${response.statusCode}');
+        // }
+      } else {
+        Get.snackbar('실패', '카카오 로그인이 취소되었거나 실패했습니다.');
+      }
+    } catch (error) {
+      Log.error('로그인 중 오류가 발생했습니다: $error');
+      Get.snackbar('오류', '로그인 중 오류가 발생했습니다: $error');
+    }
+  }
+
+  // 로그아웃 메서드 (선택 사항)
+  Future<void> kakaoLogout() async {
+    try {
+      await kakaoLoginApi.logout();
+      AppUser().clearUser();
+      user.value = AppUser();
+      Get.snackbar('성공', '로그아웃에 성공했습니다.');
+    } catch (error) {
+      Get.snackbar('실패', '로그아웃에 실패했습니다.');
+    }
+  }
+}
